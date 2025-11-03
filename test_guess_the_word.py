9100f6f1ae31716acaa6e6f28b28158a7ca26e08
unknown <ANT2305794@maricopa.edu>
2025-11-03 05:14:44 +0000

    Added game logic and tests

---
+++ test_guess_the_word.py
@@ -0,0 +1,7 @@
+# Antonita Pratcher
+# CIS256 Fall 2025
+# EX 4 Part 3
+# Due: 02 November 2025
+
+"""Program Description: This program selects a word that comes from a predefined list 
+and says if guesses are correct vs. incorrect """
