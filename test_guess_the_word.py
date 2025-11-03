c5382d9c21f1f1952dd7e58a86faf249b33aae13
unknown <ANT2305794@maricopa.edu>
2025-11-03 05:29:41 +0000

    Added game logic and tests

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -11,3 +11,12 @@ import random
 animal_list = ["alligator", "bear","camel", "dolphin", "elephant", "fox", "giraffe",
                "hyena", "iguana", "jellyfish", "koala", "lion","monkey","owl", "panda",
                "rabbit","squirrel","turtle", "vulture", "whale","zebra"]
+
+secret_animal = random.choice(animal_list)
+
+# introduction to game for user. present list of animals to user
+print(f"\nLet's play guess the secret animal!")
+print("Here is the list of animals. Guess the selected one!:")
+for animal in animal_list:
+    print(animal, end=", ")
+print("\n")
