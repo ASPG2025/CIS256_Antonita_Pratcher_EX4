04c8059b459d1d2961abad04429a4544b5eaa1f6
unknown <ANT2305794@maricopa.edu>
2025-11-03 05:21:38 +0000

    Added game logic and tests

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -5,3 +5,9 @@
 
 """Program Description: This program selects a word that comes from a predefined list 
 and says if guesses are correct vs. incorrect """
+
+import random
+
+animal_list = ["alligator", "bear","camel", "dolphin", "elephant", "fox", "giraffe",
+               "hyena", "iguana", "jellyfish", "koala", "lion","monkey","owl", "panda",
+               "rabbit","squirrel","turtle", "vulture", "whale","zebra"]
