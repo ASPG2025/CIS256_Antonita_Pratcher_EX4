854df8c9c253b5ef8ab1a9c3dd3d6f42834ed976
unknown <ANT2305794@maricopa.edu>
2025-11-03 05:36:37 +0000

    Added game logic and tests

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -8,15 +8,25 @@ and says if guesses are correct vs. incorrect """
 
 import random
 
+# list of animals that will be used 
 animal_list = ["alligator", "bear","camel", "dolphin", "elephant", "fox", "giraffe",
                "hyena", "iguana", "jellyfish", "koala", "lion","monkey","owl", "panda",
                "rabbit","squirrel","turtle", "vulture", "whale","zebra"]
 
+# program randomly chooses an animal from list
 secret_animal = random.choice(animal_list)
 
-# introduction to game for user. present list of animals to user
+# introduction to game for user. present the list of animals to user
 print(f"\nLet's play guess the secret animal!")
-print("Here is the list of animals. Guess the selected one!:")
+print("Here is the list of animals. Guess the selected one!:\n")
 for animal in animal_list:
     print(animal, end=", ")
 print("\n")
+
+# sets a variable for the number of tries to be used ain a counter
+tries_left = 8
+
+# while loop that will circle through until tries_left = 0 or animal is guessed
+while tries_left > 0:
+    print(f"\nYou have {tries_left} tries.")
+    guess = input("Guess the animal - no hints!: ").lower()
